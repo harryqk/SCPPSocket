879766e4921af8d2397df41114e49a4d01b3f4e0
harryqk <dragonqk@163.com>
2022-07-13 16:40:53 +0800

    add cross platform support

--- library.cpp
+++ library.cpp
@@ -3,5 +3,16 @@
 #include <iostream>
 
 void hello() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+#ifdef _WIN32
+    std::cout << "Hello, World win!" << std::endl;
+#elif _linux_
+    std::cout << "Hello, World linux!" << std::endl;
+#elif _APPLE_
+    std::cout << "Hello, World mac!" << std::endl;
+#elif _ANDROID_
+    std::cout << "Hello, World android!" << std::endl;
+#endif
+
 }
